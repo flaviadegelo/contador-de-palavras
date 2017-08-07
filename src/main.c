b7df892ab5b13463ca1aa4ebce7c329e7dbb853a
Flavia Degelo Camilo <flaviadegelocamilo@MacBook-Pro-de-Flavia.local>
2017-08-07 20:19:25 -0300

    codigo pronto

--- src/main.c
+++ src/main.c
@@ -1,22 +1,58 @@
 /* Contador de palavras
- *
+ * Flavia Degelo Camilo RA: 167611
  * Este programa recebera uma serie de caracteres representando palavras em sua
  * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
  * numero de palavras separadas que recebeu e, apos, encerrar.
  */
 
+
 #include <stdio.h>
 
 int main() {
-
-  char c;
-
-  c = 1;
+    char c;
+    char anterior;
+    int palavras=0;
+    int flutuante=0;
+    c = 0;
+    anterior = 0;
 
   while (c != '\n') {
-    scanf("%c", &c);
+
+    scanf("%c",&c);
+
+    if (c == ' ' || c == '.' || c == ',' || c == '?' || c == '!' || c == '-' || c == '\n' || c == ';' || c == ':'){
+
+        if((anterior >= 'a' && anterior <= 'z') || (anterior >= 'A' && anterior <= 'Z')) {
+
+            palavras++;
+      }
+
+       if(anterior >= '0' && anterior <= '9'){
+
+                if(c== '.' || c== ','){
+
+                    flutuante=1;
+                }
+                else{
+                    flutuante=0;
+                }
+
+                palavras++;
+            }
+
+
+
   }
 
-  printf("1\n");
+    anterior = c;
+
+    if(flutuante==1 && (anterior >= '0' && anterior <= '9')){
+        palavras--;
+        flutuante=0;
+}
+
+
+}
+  printf("%d\n", palavras);
   return 0;
 }
