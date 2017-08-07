e7c23087800b4aae60758cfb6a9dd47b350de000
Flavia Degelo Camilo <flaviadegelocamilo@MacBook-Pro-de-Flavia.local>
2017-08-07 20:32:57 -0300

    codigo finalizado

--- src/main.c
+++ src/main.c
@@ -20,37 +20,36 @@ int main() {
 
     scanf("%c",&c);
 
-    if (c == ' ' || c == '.' || c == ',' || c == '?' || c == '!' || c == '-' || c == '\n' || c == ';' || c == ':'){
+    if (c == ' ' || c == '.' || c == ',' || c == '?' || c == '!' || c == '-' || c == '\n' || c == ';' || c == ':'){ // A contagem de palavras eh feita pensando-se nas pontuacoes que indicam final de palavras/numeros
 
-        if((anterior >= 'a' && anterior <= 'z') || (anterior >= 'A' && anterior <= 'Z')) {
+        if((anterior >= 'a' && anterior <= 'z') || (anterior >= 'A' && anterior <= 'Z')) { // Indicacao do final de uma palavra
 
             palavras++;
-      }
+        }
 
-       if(anterior >= '0' && anterior <= '9'){
+        if(anterior >= '0' && anterior <= '9'){ // Indicacao do final de um numero ou de um ponto flutuante
 
-                if(c== '.' || c== ','){
+                if(c== '.' || c== ','){ // Se houver chances de ser ponto flutuante, ele sera testado atraves de uma flag
 
                     flutuante=1;
                 }
+
                 else{
+
                     flutuante=0;
                 }
 
                 palavras++;
-            }
+        }
 
-
-
-  }
+    }
 
     anterior = c;
 
-    if(flutuante==1 && (anterior >= '0' && anterior <= '9')){
+    if(flutuante==1 && (anterior >= '0' && anterior <= '9')){ // Caso a pontuacao ocorra devido a um ponto flutuante, ela deixa de ser considerada como uma nova palavra
         palavras--;
         flutuante=0;
-}
-
+    }
 
 }
   printf("%d\n", palavras);
